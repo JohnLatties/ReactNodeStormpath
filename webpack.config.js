dbe4546d2311754c3da675dccc261fe7ed0b2fa6
Jonathan Latties <jon.latties@gmail.com>
2016-01-31 04:02:25 -0200

    configuration of babeljs

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,10 @@ module.exports = {
     publicPath: '/js/'
   },
   module : {
-      loaders: []
+      loaders: [{
+        test: /\.js$/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'src');
+      }]
   }
 };
