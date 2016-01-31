b0c0249483544271067d74837f9ddf9439a971e2
Jonathan Latties <jon.latties@gmail.com>
2016-01-31 01:11:50 -0200

    configuration of WebPack
    	modified:   .gitignore
    	modified:   package.json
    	modified:   server.js
    	new file:   webpack.config.js
    	modified:   stormpath.yml

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
+var webpack = require('webpack');
+var config = require('./webpack.config');
 var express = require('express');
 var stormpath = require('express-stormpath');
 
-var app = express;
+var app = express();
+var compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publiPath: config.output.publicPath
+}));
 
 stormpath.init(app, {
    website: true 
@@ -12,6 +20,6 @@ app.on('stormpath.ready', function() {
       if(err) {
           return console.error(err);
       } 
-      console.log('Listening at http://localhost:3000');
+      console.log("Listening at http://localhost:3000");
    });
 });
